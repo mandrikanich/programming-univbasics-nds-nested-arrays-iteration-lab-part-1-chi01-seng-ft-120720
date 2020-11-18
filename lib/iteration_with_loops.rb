de9724a3881feb15bf00ec207e4ccdcc3933407e
Michael Andrikanich <mandrikanich@gmail.com>
2020-11-18 18:42:31 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,7 +1,7 @@
 def find_even_values(src)
   row_index = 0 
   while row_index < src.count do 
-    element_idex = 0 
+    element_index = 0 
     while element_index < src[row_index].count do
       if src[row_index][element_index].even?
         p src[row_index][element_index]
